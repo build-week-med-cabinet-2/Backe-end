d0e6eae5d0549e7b7c56f47543c4ecea19da1b0a
CScori <49799016+CScori@users.noreply.github.com>
2019-11-15 19:19:44 -0600

    adds medical tble not run

---
+++ database/migrations/20191115190926_medicinal.js
@@ -0,0 +1,38 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('strains', t => {
+      t.increments('strain_id')
+      t.string('strain_name', 128)
+        .notNullable()
+    t.string('strain_type' 128)
+        .notNullable()
+    t.string('strain_desc', 500)
+    t.string('benefits', 500)
+  })
+  .createTable('dosages', t => {
+      t.increments('dosage_id')
+     // dose string
+     // usage string
+  })
+  .createTable('strain_dosage', t => {
+      t.increments('direction_id')
+      t.integer('strain_id')
+        .unsigned()
+        .notNullabble()
+        .refrences('strain_id')
+        .inTable('strains')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    t.integer('dosage_id')
+        .unsigned()
+        .notNullabble()
+        .refrences('dosage_id')
+        .inTable('dosages')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  
+};
