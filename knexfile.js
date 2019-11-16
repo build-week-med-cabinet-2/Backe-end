ffd2f149a03d358ade2ae02a2aa88bee4574ee0d
CScori <49799016+CScori@users.noreply.github.com>
2019-11-15 18:30:12 -0600

    changed file name

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    connection: { filename: './database/medcab.db3' },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
