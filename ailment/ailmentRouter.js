6f5fa7825c26a276745a1674d985515a252905ca
Luis Mendes <cvlopes88@gmail.com>
2019-11-19 16:31:50 -0500

    last route change

--- ailment/ailmentRouter.js
+++ ailment/ailmentRouter.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const ailments = require('./ailmentModel');
-const restricted = require('../auth/authenticate-middleware');
+const restricted = require('.././api/auth/authenticate-middleware');
 
 router.get('/', restricted, (req, res) => {
   ailments.find()
