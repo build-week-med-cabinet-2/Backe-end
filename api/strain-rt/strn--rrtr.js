166578e6031cf70e4120ac1f7467ff3667a71c4e
Luis Mendes <cvlopes88@gmail.com>
2019-11-22 17:36:14 -0500

    fixes

--- api/strain-rt/strn--rrtr.js
+++ api/strain-rt/strn--rrtr.js
@@ -20,9 +20,9 @@ strn.get('/:id', restricted, (req, res) => {
 strn.post('/', (req, res) => {
     console.log(req.body)
 
-const {strain_name, strain_type, strain_desc} = req.body
-const strain = {strain_name, strain_type, strain_desc}
-const {effects, flavor} = req.body
+const {strain_name, strain_type, strain_desc} = req.body;
+const strain = {strain_name, strain_type, strain_desc};
+const {effects, flavor} = req.body;
 
 
 if(!strain_name || !strain_type) {
@@ -43,31 +43,7 @@ if(!strain_name || !strain_type) {
             res.status(201).json(strain)
         })
         .catch(err => res.status(500).json({ error: `internal service issue see: ${err}`}))
-}
-
-// strn.put('/:id', async (req, res) => {
-//         const edit = req.body
-//         const { id } = req.params
-//         if (!edit.strain_name || !edit.strain_type) {
-//             return res.status(400).json({message: `no strain with id of ${id}found`})
-//         }
-//         try {
-//             const str = await helper.findById(id)
-           
-//             if (!str) {
-//                 res.status(404).json({message: `no strain with id of ${id}found`})
-//             } else {
-//                 await helper.update(id, edit)
-//                 const uS = await db.findById(id)
-    
-//                 res.status(200).json(uS)
-//             }
-//         } catch (err) {
-//             res.status(500)
-//             .json({ error: 'failed to get users from db'})
-        
-//         }
-// })
+};
 
 strn.delete('/:id', async (req, res) => {
     const id = req.params.id
@@ -77,35 +53,17 @@ strn.delete('/:id', async (req, res) => {
             res.status(200).json({message: `Strain with id of ${id} has been removed`, id: id})
         } else {
             res.status(404).json({errorMessage: `Strain with ${id} could not be found`})
-        }
+        };
         
     })
     .catch(err => {
         res.status(500)
         .json({ error: 'failed to delete strain from db'})
-        })
-})
+        });
+    })
+});
 
 
 
 
 module.exports = strn;
-strn.delete('/:id', async (req, res) => {
-    const id = req.params.id
-    helper.remove(id)
-    .then(rem => {
-        if (rem > 0) {
-            res.status(200).json({message: `Strain with id of ${id} has been removed`, id: id})
-        } else {
-            res.status(404).json({errorMessage: `Strain with ${id} could not be found`})
-        }
-        
-    })
-    .catch(err => {
-        res.status(500)
-        .json({ error: 'failed to delete strain from db'})
-        })
-})
-
-module.exports = strn
-
